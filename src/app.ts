e75225d919ac6c8d27a965c303bcea43d1a66c16
Tiago Gomes <tidgomes@gmail.com>
2023-04-27 12:57:08 -0300

    create decorator for title propery

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,10 @@
-function Logger(stringParam: string) {
-  console.log('LOGGER FACTORY')
-  return function (constructor: Function) {
-    console.log(stringParam)
-    console.log(constructor)
-  }
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator')
+  console.log(target, propertyName)
 }
-
-function WithTemplate(template: string, hookId: string) {
-  console.log('TEMPLATE FACTORY')
-  return function (_: Function) {
-    console.log('WITH TEMPLATE TEST')
-    const hookEl = document.getElementById(hookId)
-    if (hookEl) hookEl.innerHTML = template
-  }
-}
-
-@WithTemplate('<h1>Test</h1>', 'app')
-@Logger('LOGGER')
-class Person {
-  name = 'Mano'
-
-  constructor() {
-    console.log('Creating person object...')
-  }
-}
-
-// const pers = new Person()
-
-// console.log(pers)
 
 class Product {
+  @Log
   title: string
   private _price: number
 
