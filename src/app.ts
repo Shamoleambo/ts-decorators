e7e4a551d2228b73e474ffc9cfaebeb5eeb13618
Tiago Gomes <tidgomes@gmail.com>
2023-04-27 12:40:48 -0300

    check the order in which the decorators and factories run

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
+function Logger(stringParam: string) {
+  console.log('LOGGER FACTORY')
+  return function (constructor: Function) {
+    console.log(stringParam)
+    console.log(constructor)
+  }
+}
+
 function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY')
   return function (_: Function) {
+    console.log('WITH TEMPLATE TEST')
     const hookEl = document.getElementById(hookId)
     if (hookEl) hookEl.innerHTML = template
   }
 }
 
 @WithTemplate('<h1>Test</h1>', 'app')
+@Logger('LOGGER')
 class Person {
   name = 'Mano'
 
