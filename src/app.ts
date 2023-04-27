f50cba88ffe769857dcd4dbd320f3d85c9a2cc0b
Tiago Gomes <tidgomes@gmail.com>
2023-04-27 13:01:27 -0300

    add decorator to set accessor

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,23 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName)
 }
 
+function LogAccessor(
+  target: any,
+  name: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Accessor Decorator')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
 class Product {
   @Log
   title: string
   private _price: number
 
+  @LogAccessor
   set price(value: number) {
     if (value > 0) this._price = value
     else throw new Error('Value must be grater than zero')
